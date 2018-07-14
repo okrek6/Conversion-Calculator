3e180451dcb4f07ee096f6759327bf437c912cc7
Brendan Krekeler <okrek6@gmail.com>
2018-07-13 20:36:41 -0700

    First commit

---
+++ Conversion Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conversion Calculator
+//
+//  Created by Brendan Krekeler on 7/13/18.
+//  Copyright © 2018 Brendan Krekeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
